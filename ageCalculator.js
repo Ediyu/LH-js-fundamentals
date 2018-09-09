dd42ecf5b8613b26498a55985463d8f1e511d5d1
Ggabju <peter.h.9002@gmail.com>
2018-09-09 17:14:18 +0000

    adding the additional excercises

---
+++ ageCalculator.js
@@ -0,0 +1,6 @@
+function ageCalculator(name, birthYear, currentYear) {
+    return name + " " + (birthYear-currentYear);
+}
+
+console.log(ageCalculator("Peter", 1990, 2018));
+
---
+++ hundred.js
@@ -0,0 +1,8 @@
+function howManyHundreds(num) {
+    return (num/100) - ((num%100)/100);
+}
+
+console.log(howManyHundreds(1000));
+console.log(howManyHundreds(520));
+console.log(howManyHundreds(0));
+console.log(howManyHundreds(999));
