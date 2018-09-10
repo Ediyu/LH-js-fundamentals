ec848e831914a9e9c712e36c181fdb6c62a09534
Ggabju <peter.h.9002@gmail.com>
2018-09-10 04:02:34 +0000

    adding others

---
+++ rangeNumber.js
@@ -0,0 +1,10 @@
+function range(start, end, step) {
+  var num = [];
+  for (var i = start; i <= end; i += step){
+    num.push(i);
+  }console.log(num);
+}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
